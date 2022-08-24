75dd83035f5f04d3b48e958b6ddcec0dc4cfe90c
Vitor Rossignolli <vitorrossignolli@gmail.com>
2022-08-24 20:35:16 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Vitor Rossignolli on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
